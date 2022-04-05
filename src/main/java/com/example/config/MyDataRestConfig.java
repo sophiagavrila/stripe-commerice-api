570b0342a6d89bf296b0a992e6052a47a1f67438
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2022-04-05 12:41:26 -0400

    add notes on paging & sorting fucntioanloty retained with MyDataRestConfig class

--- src/main/java/com/example/config/MyDataRestConfig.java
+++ src/main/java/com/example/config/MyDataRestConfig.java
@@ -11,22 +11,28 @@ import com.example.models.ProductCategory;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 	
-//    @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+	/**
+	 * Currently this is configured for GET requests (READ only) 
+	 * Alternatively, we could have built a RestController, but then we lose
+	 * Spring Data REST support for Paging & Sorting
+	 * 
+	 * https://docs.spring.io/spring-data/rest/docs/2.0.0.M1/reference/html/paging-chapter.html
+	 */
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) { // this is from Spring Data Rest
 
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
         // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .forDomainType(Product.class) // a.k.a Apply to Repo for: ProductCategory
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)) // single item
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)); // collection
 
         // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .forDomainType(ProductCategory.class) // a.k.a Apply to Repo for: ProductCategory
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)) // single item
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)); // collection
 
     }
 
